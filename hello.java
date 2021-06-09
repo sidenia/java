a06ab6ad2eb9a73192bd29635a9fbb1ca48ae851
sidenia <sideniabezerra@hotmail.com>
2021-06-09 17:02:42 -0300

    hello

--- hello.java
+++ hello.java
@@ -3,9 +3,9 @@ package dayone;
 public class Hello {
 
 	public static void main(String[] args) {
-		
+		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		//o ln leva o texto para a pr√≥xima linha, j√° o print n√£o. veja:
+		//o ln leva o texto para a prÛxima linha, j· o print n„o. veja:
 		System.out.println("Oie, ");
 		System.out.println("Eu me chamo");
 		System.out.println("Sidenia");
